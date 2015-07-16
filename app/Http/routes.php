3867817682acd560e181f1bd7356dfacc93e9c18
David Muggleton <david@vibesfamily.com>
2015-07-16 20:23:30 +0100

    Add route for movie links so angular is loaded instead of an error

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,3 +7,8 @@ Route::group(['prefix' => 'api/v1'], function () {
 Route::get('/', function () {
 	return view('index');
 });
+// If they land on a movie
+Route::get('{id}/{name}', function ($id, $name) {
+	return view('index');
+})
+->where(['id' => '[0-9]+');
