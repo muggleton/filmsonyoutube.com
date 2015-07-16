93a12967109634a80566c3c7c4e0cfc8d6360036
David Muggleton <david@vibesfamily.com>
2015-07-16 18:25:58 +0100

    Update front end dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -42,37 +42,29 @@ elixir.extend('mysass', function(src, name, output, options) {
 });
 
 var paths = {
-	'jquery': './resources/assets/bower/jquery/',
 	'bootstrap': './resources/assets/bower/bootstrap-sass-official/assets/',
-	'angular': './resources/assets/bower/angular/',
-	'angular_loading_bar': './resources/assets/bower/angular-loading-bar/src/',
-	'angular_timeago': './resources/assets/bower/angular-timeago/src/',
 	'angular_route': './resources/assets/bower/angular-route/',
-	'bootstrap_sidebar': './resources/assets/bower/bootstrap-sidebar/',
-	'bootstrap_slider': './resources/assets/bower/seiyria-bootstrap-slider/dist/'
 }
 
 elixir(function(mix) {
 	mix.registerWatcher('mysass', '**/*.scss')
 	.mysass('resources/assets/sass/app.scss', 'app.css', 'public/assets/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
-	.copy(paths.bootstrap_sidebar + '/dist/js/sidebar.js', 'public/assets/js/vendor/sidebar.js')
 	// Copy fonts to public
 	.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/assets/fonts/bootstrap')
-	// Copy bootstrap dependancy
-	.copy(paths.jquery + 'dist/jquery.min.js', 'public/assets/js/vendor/jquery.min.js')
-	// Copy bootstrap dependancy
-	.copy(paths.bootstrap +  'javascripts/bootstrap.min.js', 'public/assets/js/vendor/bootstrap.min.js')
-	.copy(paths.bootstrap_slider + 'bootstrap-slider.min.js', 'public/assets/js/vendor/slider.js')
 	// Copy angular route as it needs to be kept seperate from angular...
 	.copy(paths.angular_route + 'angular-route.min.js', 'public/assets/js/vendor/angular-route.min.js')
-	// Combine angular and angular vendor scripts
+	// Combine vendor/angular and angular vendor scripts
 	.scripts([
+
+		'bootstrap-sidebar/dist/js/sidebar.js',
+		'jquery/dist/jquery.min.js',
+		'bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
 		'angular/angular.js', 
 		'angular-loading-bar/src/loading-bar.js',
 		'ngInfiniteScroll/build/ng-infinite-scroll.min.js',
 		'angularjs-slider/dist/rzslider.min.js',
 		'ng-debounce/angular-debounce.js'
-		], 'public/assets/js/vendor/angular.js' ,'resources/assets/bower')
+		], 'public/assets/js/vendor/vendor.js' ,'resources/assets/bower')
 	// Combine all scripts (controllers, filters etc) in to one app.js file
 	.scriptsIn(['resources/assets/js'], 'public/assets/js/app.js')
 });
